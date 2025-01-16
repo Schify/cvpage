23811708cc2739493fcf6b9a5d3f5427ea91287f
reonokiy <reonokiy@gmail.com>
2025-01-16 11:49:07 +0800

    update language links, add project link

--- src/utils/cv.ts
+++ src/utils/cv.ts
@@ -1,12 +1,23 @@
 import type { CV, BaseInfo } from './types';
 import { micromark } from 'micromark';
 
+export function extractLangInfo(id: string, lang?: string) {
+    return lang || id;
+}
+
 export function extractBaseInfo(id: string, cv: CV): BaseInfo {
     return {
         name: cv.name,
-        lang: cv.lang || id,
+        lang: extractLangInfo(id, cv.lang),
         info: cv.info,
     };
+}
+
+export function extractLanguageLink(id: string, cv: CV) {
+    return {
+        lang: extractLangInfo(id, cv.lang),
+        link: `/${id}`,
+    }
 }
 
 export function md2html(md: string): string {
